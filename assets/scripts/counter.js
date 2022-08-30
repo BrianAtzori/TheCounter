e80466e37dd7df23a5e4d16f12fbd9206036fe6b
BrianAtzori <brianatzori@gmail.com>
2022-08-30 18:10:47 +0200

    Javascript Troubleshooting in counter.js

---
+++ assets/scripts/counter.js
@@ -0,0 +1,19 @@
+class Counter{
+
+  constructor(currentNumber){
+    this.currentNumber = currentNumber
+  }
+
+}
+
+const minusSign = document.querySelector('minus-sign')
+
+const displayedNumber = document.querySelector('displayed-number')
+
+const plusSign = document.querySelector('plus-sign')
+
+minusSign.addEventListener('click', button => {
+
+    alert("ciao")
+
+})

1d3e47eb5db6c052664857c9f1779d0792b8efbe
BrianAtzori <brianatzori@gmail.com>
2022-08-31 11:31:38 +0200

    Basic Counter Function

--- assets/scripts/counter.js
+++ assets/scripts/counter.js
@@ -1,19 +1,62 @@
+
+//-----CLASSES----------------------------------------------------
+
 class Counter{
 
+  // Counter class - Used to decrease or increase the displayed value, even with multipliers
+
   constructor(currentNumber){
-    this.currentNumber = currentNumber
+    this.currentNumber = parseFloat(currentNumber)
+  }
+
+  modifyValue(operator,currentNumber){
+
+    if(operator === '+')
+    {
+      this.currentNumber = parseFloat(currentNumber) + 1
+    }
+    else
+    {
+      this.currentNumber = parseFloat(currentNumber - 1
+    }
+
+  }
+
+  modifyDisplayedNumber()
+  {
+    displayedNumber.innerHTML=this.currentNumber.toString()
   }
 
 }
 
-const minusSign = document.querySelector('minus-sign')
+//--------GETELEMENTS-----------------------------------------------
 
-const displayedNumber = document.querySelector('displayed-number')
 
-const plusSign = document.querySelector('plus-sign')
+const minusSign = document.querySelector('.minus-sign')
+
+const displayedNumber = document.querySelector('.displayed-number')
+
+const plusSign = document.querySelector('.plus-sign')
+
+
+const counter = new Counter (displayedNumber.innerHTML)
+
+
+
+//--------EVENTS------------------------------------------------
+
+
 
 minusSign.addEventListener('click', button => {
 
-    alert("ciao")
+  counter.modifyValue(minusSign.innerHTML, displayedNumber.innerHTML)
+  counter.modifyDisplayedNumber()
 
-})
+});
+
+plusSign.addEventListener('click', button => {
+
+  counter.modifyValue(plusSign.innerHTML, displayedNumber.innerHTML)
+  counter.modifyDisplayedNumber()
+
+});
---
+++ assets/scripts/value-manager.js
@@ -0,0 +1,3 @@
+
+
+//Aggiungere funzione per restartare
